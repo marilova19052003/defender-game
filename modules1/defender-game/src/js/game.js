0275a220b4290e28f84713ef586b1c4928c9cc66
Mariana <marilovamariana@gmail.com>
2024-11-26 20:24:12 +0300

    Задание выполнено

---
+++ modules1/defender-game/src/js/game.js
@@ -0,0 +1,22 @@
+import Character from "./domain.js";
+
+class Game {
+  start() {
+    console.log("game started");
+  }
+}
+
+class GameSavingData {
+  // Логика сохранения игры
+}
+
+function readGameSaving() {
+  // Логика чтения сохранения игры
+}
+
+function writeGameSaving() {
+  // Логика записи сохранения игры
+}
+
+export default Game;
+export { GameSavingData, readGameSaving, writeGameSaving };
---
+++ modules1/defender-game/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: "source-map",
+  mode: "development",
+};
